7f73ca1389b48c3634c5ecf4788dacde7af3e570
Frédéric Rechtenstein <frec@google.com>
2021-12-07 17:02:11 +0900

    Capture Bazel test logs

--- ci/kokoro/build.sh
+++ ci/kokoro/build.sh
@@ -26,6 +26,19 @@ set -e
 #  parameters, will print the full command, with credentials, in the build logs.
 # set -x
 
+capture_test_logs() {
+  # based on http://cs/google3/third_party/fhir/kokoro/common.sh?rcl=211854506&l=18
+  mkdir -p "$KOKORO_ARTIFACTS_DIR"
+  # copy all test.log and test.xml files to the kokoro artifacts directory
+  find -L bazel-testlogs -name "test.log" -o -name "test.xml" -exec cp --parents {} "$KOKORO_ARTIFACTS_DIR" \;
+  # Rename the copied test.log and test.xml files to sponge_log.log and sponge_log.xml
+  find -L "$KOKORO_ARTIFACTS_DIR" -name "test.log" -exec rename 's/test.log/sponge_log.log/' {} \;
+  find -L "$KOKORO_ARTIFACTS_DIR" -name "test.xml" -exec rename 's/test.xml/sponge_log.xml/' {} \;
+}
+
+# Run capture_test_logs when the script exits
+trap capture_test_logs EXIT
+
 pip3 install --requirement "requirements.txt"
 
 bazel test --python_path=$PYTHON_BIN --test_verbose_timeout_warnings //tensorflow_model_optimization/python/core/...
